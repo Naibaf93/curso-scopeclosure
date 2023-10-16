9140e3e9960ed1a1ee395177b3769a8f55dd75fd
Fabián Huertas <miloh9318@hotmail.com>
2023-10-16 10:28:47 -0500

    creando el archivo y la carpeta hoisting

---
+++ src/hoisting/hoisting.js
@@ -0,0 +1,13 @@
+//var nameOfDog; // undefined
+//console.log(nameOfDog); //<-- El valor saldra undefined
+//var nameOfDog = 'Draco'; 
+//console.log(nameOfDog);//<-- ahora la consola si nos mostrara el nombre de draco
+
+var draco; // undefined
+nameOfDog();
+
+function nameOfDog() {  
+    console.log('el mejor perrito es ${draco}');
+}
+
+var draco = 'draquito';
