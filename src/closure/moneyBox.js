39b00c7f759ae0c8e80a0ba9486af2cf9fca645c
Fabián Huertas <miloh9318@hotmail.com>
2023-10-15 20:21:50 -0500

    creando un closure para almacenar datos de mascotas

--- src/closure/moneyBox.js
+++ src/closure/moneyBox.js
@@ -29,3 +29,20 @@ const moneyBoxFabian = moneyBox();
 moneyBoxFabian(10);
 moneyBoxFabian(20);
 moneyBoxFabian(5);
+
+//  ejercicio ejemplo donde creamos closure para almacenar datos de mascotas
+export function createPetList() {
+    const petList = [];
+    return function (newPet) {
+      if (newPet) {
+        petList.push(newPet)
+      }
+      return petList;
+    }
+  }
+  
+  const myPetList = createPetList();
+  
+  console.log(myPetList("michi"));
+  console.log(myPetList("firulais"));
+  console.log(myPetList());
